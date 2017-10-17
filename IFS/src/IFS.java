6e480c24d848e6c14e9c7afa74d59007f452e686
Michael Wulber <mwulber2015@ufl.edu>
2017-10-17 09:28:13 -0400

    submitted version

--- IFS/src/IFS.java
+++ IFS/src/IFS.java
@@ -7,8 +7,8 @@ import java.util.LinkedList;
 import java.util.Random;
 
 public class IFS {
-    private final int DEPTH_THRESHHOLD = 100;
-    private final int ITERATIONS = 1000000;
+    private int DEPTH_THRESHHOLD;
+    private int ITERATIONS;
 
     private LinkedList<TransformWrapper> transforms;
     private Color foreground;
@@ -20,6 +20,9 @@ public class IFS {
     // default values
     public IFS(){
         this.image = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_ARGB);
+        this.DEPTH_THRESHHOLD = 100;
+        this.ITERATIONS = 1000000;
+
         this.foreground = Color.BLACK;
         this.background = Color.WHITE;
     }
@@ -31,12 +34,11 @@ public class IFS {
 
         // debug
         for (TransformWrapper tw : transforms) {
-            System.out.println(tw.getTransform().toString());
+            System.out.println(tw.toString());
         }
 
         Point2D.Double point = randomPoint();
         Point2D.Double temp = point;
-        System.out.println("(x, y): (" + point.x + ", " + point.y + ")" );
         AffineTransform at;
 
         g2d.setColor(foreground);
@@ -44,8 +46,6 @@ public class IFS {
             at = randomAffineTransform();
             at.transform(point, temp);
             point = temp;
-            g2d.draw(new Line2D.Double(mapX(point.x), mapY(point.y),
-                    mapX(point.x), mapY(point.y)));
         }
 
         for (int i = 0; i < ITERATIONS; i++){
@@ -72,7 +72,6 @@ public class IFS {
         for (TransformWrapper tw : transforms){
             sum += tw.getProbability();
             if (num < sum){
-                // System.out.println(count);
                 this.distribution[count]++;
                 return tw.getTransform();
             }
@@ -85,7 +84,7 @@ public class IFS {
     // return actual distributions
     private void printDistribution() {
         for (int i = 0; i < distribution.length; i++) {
-            System.out.println("dist: " + (double)distribution[i]/(double)ITERATIONS);
+            System.out.println("dist: " + (double)distribution[i]/(double)(ITERATIONS + DEPTH_THRESHHOLD));
         }
     }
 
@@ -111,7 +110,6 @@ public class IFS {
         for (int i = 0; i < distribution.length; i++){
             distribution[i] = 0;
         }
-        System.out.println(transforms.size());
         this.transforms = transforms;
     }
 
@@ -126,4 +124,22 @@ public class IFS {
     public void setImage(BufferedImage image) {
         this.image = image;
     }
+
+    public Color getForeground() {
+        return foreground;
+    }
+
+    public Color getBackground() {
+        return background;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setGenerations(int generations) {
+        this.DEPTH_THRESHHOLD = generations/10;
+        this.ITERATIONS = generations - DEPTH_THRESHHOLD;
+    }
 }
+
