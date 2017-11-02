0fbea08ac14e9a67e99ba042d4da5d6f54732c67
Michael Wulber <mwulber2015@ufl.edu>
2017-11-02 18:45:25 -0400

    added Java3D Tutorial

---
+++ Java3D_Tutorial/ApplicationWindow.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class ApplicationWindow extends JFrame{
+    private final Display panel;
+
+    public ApplicationWindow() {
+        // frame attributes
+        this.setTitle("3D Tutorial - Michael Wulber");
+        this.setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
+
+        this.panel = new Display();
+        this.getContentPane().add(panel, BorderLayout.CENTER);
+
+        // add a menu to the frame
+        addMenu();
+    }
+
+    private void addMenu(){
+        // setup the frame's menu bar
+
+        // File menu
+        JMenu fileMenu = new JMenu("File");
+
+        // Exit
+        JMenuItem exitItem = new JMenuItem("Exit");
+        exitItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        fileMenu.add(exitItem);
+
+        // attach menu to a menu bar
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(fileMenu);
+        this.setJMenuBar(menuBar);
+    }
+}
---
+++ Java3D_Tutorial/Launcher.java
@@ -0,0 +1,25 @@
+// Michael Wulber
+// CAP 3027
+// Java 3D Tutorial
+
+import javax.swing.*;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    private static void createAndShowGUI(){
+        ApplicationWindow window = new ApplicationWindow();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.pack();
+        window.setVisible(true);
+    }
+
+}
