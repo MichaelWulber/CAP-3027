a231934ea19eddd00f61af3cfc60f25d0eacb911
Michael Wulber <mwulber2015@ufl.edu>
2017-11-02 18:51:32 -0400

    syncing with lappy

--- Java3D_Tutorial/Display.java
+++ Java3D_Tutorial/Display.java
@@ -1,5 +1,19 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Display extends JPanel {
+    public BufferedImage image;
+    public Graphics2D g2d;
+
+    Display(int width, int height){
+        this.image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        this.g2d = image.createGraphics();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, this);
+    }
 
 }
